dcb2488b9aa0265ccf655b1564be823219ff4cbb
K4-pi <Kap1_priv@proton.me>
2026-03-21 14:08:39 +0100

    First approach of heap memory allocator

---
+++ include/memory_alloc.h
@@ -0,0 +1,9 @@
+#ifndef _MEMORY_ALLOC_H
+#define _MEMORY_ALLOC_H
+
+#include <stddef.h>
+
+void *allocate(size_t size);
+void heap_info();
+
+#endif
---
+++ src/memory_alloc.c
@@ -0,0 +1,93 @@
+#include <stddef.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <stdbool.h>
+
+#include "memory_alloc.h"
+
+static void *get_block(size_t size);
+static void *add_block(size_t size);
+
+typedef struct {
+	size_t size;
+	void *next;
+	_Bool is_free;
+} Block; 
+
+void *heap_start = NULL;
+void *heap_end   = NULL;
+Block *last_block = NULL;
+
+static void *get_block(size_t size) {
+	
+	Block *current = heap_start;
+
+	while (current->next != NULL && current != heap_end) {
+	
+		if (current->is_free && current->size >= size)
+			return current;
+
+		current = current->next; 
+	}
+
+	return add_block(size);
+}
+
+static void *add_block(size_t size) {
+
+	if ((size_t)(heap_end - (void *)last_block) < size) {
+		printf("Not enough memory on page!\n");
+		return NULL;
+	}
+
+	Block *new_block = (Block *)((char *)(last_block + 1) + last_block->size);
+
+	// new_block->size = size;
+	// new_block->is_free = false;
+	// new_block->next = NULL;
+
+	last_block->next = new_block;
+	last_block = new_block;
+
+	return new_block;
+}
+
+void *allocate(size_t size) {
+
+	// First use of allocate
+	if (heap_start == NULL) {
+		heap_start = sbrk(4096);
+		heap_end = sbrk(0);
+
+		if (heap_start == (void *)-1) {
+			perror("sbrk error");
+			heap_start = NULL;
+			return (void *)-1;
+		}
+		
+		Block *block = heap_start;
+		last_block = heap_start;
+
+		block->size = size;
+		block->is_free = false;
+
+		return (void *)(block + 1);
+	}
+
+	Block *block = get_block(size);
+
+	if (block == (void *)-1) return NULL;
+	
+	block->size = size;
+	block->is_free = false;
+
+	return (void *)(block + 1); 
+}
+
+void heap_info() {
+	printf("\nheader size = %lu\n\n", sizeof(Block));
+	printf("Heap start = %p\n", heap_start);
+	printf("Heap end   = %p\n", heap_end);
+	printf("last block = %p\n\n", last_block);
+}
