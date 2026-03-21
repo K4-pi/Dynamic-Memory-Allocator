d66e0e05c4c0c9e69001cfe8227aaef2673801b7
K4-pi <Kap1_priv@proton.me>
2026-03-21 14:23:47 +0100

    allocate doc

--- include/memory_alloc.h
+++ include/memory_alloc.h
@@ -3,7 +3,16 @@
 
 #include <stddef.h>
 
+/**
+ * @brief Allocate a block of memory on the heap
+ *
+ * @param size -> Size of allocated memory in bytes
+ *
+ * @return On success address of allocated memory, else (void *)-1 on error
+ *
+ */
 void *allocate(size_t size);
+
 void heap_info();
 
 #endif
